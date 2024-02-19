e59c50c2ae84a1374eb09576f6daf424e852e6c6
wsqstar <wsq_star@outlook.com>
2024-02-19 10:48:52 +0800

    refactor: 🎨 贴合2SO4SAI算法实现

--- pulp-p-median.py
+++ pulp-p-median.py
@@ -6,14 +6,15 @@ from scipy.spatial.distance import cdist
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def p_median_algorithm(SouthCarolina_shp, P_RELOCATED=3):
+def p_median_algorithm(SouthCarolina_shp, P_LOCATED=3,fixed_facilities=[]):
 
     """
     P-Median algorithm implementation.
 
     Parameters:
         SouthCarolina_shp (geopandas.GeoDataFrame): GeoDataFrame containing shapefile data.
-        P_RELOCATED (int): Number of facilities to be placed.
+        P_LOCATED (int): Number of facilities to be placed.
+        fixed_facilities (list): List of indices of facilities to be fixed.
 
     Returns:
         rslt (list): List of indices of facility nodes.
@@ -21,7 +22,7 @@ def p_median_algorithm(SouthCarolina_shp, P_RELOCATED=3):
     """
     # Create demand and facilities variables
     demand = np.arange(0, 46, 1)
-    facilities = np.arange(0, 20, 1)
+    facilities = np.arange(0, 46, 1)
 
     # Calculate distance matrix
     coords = list(zip(SouthCarolina_shp.centroid.x, SouthCarolina_shp.centroid.y))
@@ -42,12 +43,15 @@ def p_median_algorithm(SouthCarolina_shp, P_RELOCATED=3):
     prob += sum(sum(h[i] * d[i][j] * Y[i][j] for j in facilities) for i in demand)
 
     # Constraints
-    prob += sum([X[j] for j in facilities]) == P_RELOCATED
+    prob += sum([X[j] for j in facilities]) == P_LOCATED
     for i in demand:
         prob += sum(Y[i][j] for j in facilities) == 1
     for i in demand:
         for j in facilities:
-            prob += Y[i][j] <= X[j]
+            if j in fixed_facilities:
+                prob += Y[i][j] == 1  # 将固定的设施的分配变量设为1
+            else:
+                prob += Y[i][j] <= X[j]
 
     # Solve the problem
     prob.solve()
@@ -68,12 +72,8 @@ def p_median_algorithm(SouthCarolina_shp, P_RELOCATED=3):
     return rslt, fac_loc
 
 
-
-
-
-
-
 def main():
+    # Step1 整理数据
     #read a sample shapefile SC一共46个县
     SouthCarolina_shp = gp.read_file("./gadm41_USA_shp/gadm41_USA_SC_countries.shp") 
     # 读取 CSV 文件 
@@ -83,11 +83,23 @@ def main():
     SouthCarolina_shp['NAME_2'] = SouthCarolina_shp['NAME_2'].str.upper()
     SouthCarolina_shp = pd.merge(SouthCarolina_shp, csv_data, left_on='NAME_2', right_on='COUNTY', how='left')
 
+    # Step2 调用pulp并求解
     # 调用函数并绘制结果
-    rslt, fac_loc = p_median_algorithm(SouthCarolina_shp)
+    # P_LOCATED是重新选址的数目 fixed_facilities = [0, 1, 2] (例)的数量
+    TOTAL_FACILITIES = 6
+    fixed_facilities = [0, 1, 2]  # 例如，固定前三个设施不动
+    P_LOCATED= TOTAL_FACILITIES - len(fixed_facilities)
+    rslt, fac_loc = p_median_algorithm(SouthCarolina_shp,P_LOCATED,fixed_facilities)
+    print(f'Serial number of site selection {rslt}' )
+    #Step3 可视化
     fig, ax = plt.subplots(figsize=(5, 5))
+    ax.set_title(f'Serial number of site selection {rslt}')  # 修改标题
     SouthCarolina_shp.centroid.plot(ax=ax, markersize=SouthCarolina_shp['2020ESTIMATES'] / 1000)
-    fac_loc.centroid.plot(ax=ax, color="red", markersize=300)
+    fac_loc.centroid.plot(ax=ax, color="red", markersize=300,marker="*", label="New Facilities")
+    # 显示被固定设施的位置
+    fixed_facility_locs = SouthCarolina_shp.iloc[fixed_facilities]
+    fixed_facility_locs.centroid.plot(ax=ax, color="blue", markersize=300, marker="*", label="Fixed Facilities")
+    plt.legend()
     plt.show()
 
 # 当直接执行这个脚本文件时，main 函数将会被执行
